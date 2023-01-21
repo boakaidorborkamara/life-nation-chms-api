427701e650cba1b8f6dd6e61379a855fdb5cb147
Boakai Dorbor Kamara <boakaidkamara@gmail.com>
2023-01-21 20:14:35 +0000

    create specific endpoits for people router

--- routers/people-router.js
+++ routers/people-router.js
@@ -14,4 +14,14 @@ router.get('/v1/people', peopleController.people_list);
 //get person detail
 router.get('/v1/person/:id', peopleController.person_details);
 
+
+//edit person details
+router.put('/v1/person/:id', peopleController.person_edit);
+
+
+//delete person
+router.delete('/v1/person/:id', peopleController.person_delete);
+
+
+
 module.exports = router;
