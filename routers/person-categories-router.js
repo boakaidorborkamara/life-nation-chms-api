c5b55b3e040d8a951386b9258f1d7bba243e5b09
Boakai Dorbor Kamara <boakaidkamara@gmail.com>
2023-01-29 11:28:23 +0000

    Refactor endpoints in PersoCategoriesController file

--- routers/person-categories-router.js
+++ routers/person-categories-router.js
@@ -1,26 +1,26 @@
 const express = require('express');
 const router = express.Router();
-const familyController = require('../controller/familyController');
+const personCategoriesController = require('../controller/personCategoriesController');
 
 
 //create new preson
-router.post('/v1/person-categories', familyController.family_create);
+router.post('/v1/person-categories', personCategoriesController.person_category_create);
 
  
 //list all families
-router.get('/v1/person-categories', familyController.families_list);
+router.get('/v1/person-categories', personCategoriesController.person_category_list);
 
 
 //get family detail
-router.get('/v1/person-categories/:id', familyController.family_details);
+// router.get('/v1/person-categories/:id', familyController.family_details);
 
 
 //edit family details
-router.put('/v1/person-categories/:id', familyController.family_edit);
+router.put('/v1/person-categories/:id', personCategoriesController.person_category_edit);
 
 
 //delete family
-router.delete('/v1/person-categories/:id', familyController.family_delete);
+router.delete('/v1/person-categories/:id', personCategoriesController.person_category_delete);
 
 
 
