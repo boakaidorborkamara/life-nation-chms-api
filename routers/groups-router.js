b6c640b2b1dd4fddf43924257b04f9d4c3b063b0
Boakai Dorbor Kamara <boakaidkamara@gmail.com>
2023-01-29 14:56:13 +0000

    Refactor code in group-router.js file

--- routers/groups-router.js
+++ routers/groups-router.js
@@ -1,26 +1,26 @@
 const express = require('express');
 const router = express.Router();
-const familyController = require('../controller/familyController');
+const groupsController = require('../controller/groupsController');
 
 
-//create new preson
-router.post('/v1/families', familyController.family_create);
+//create new group
+router.post('/v1/groups', groupsController.groups_create);
 
  
-//list all families
-router.get('/v1/families', familyController.families_list);
+//list all group
+router.get('/v1/groups', groupsController.groups_list);
 
 
-//get family detail
-router.get('/v1/families/:id', familyController.family_details);
+//get groups detail
+router.get('/v1/groups/:id', groupsController.groups_edit);
 
 
-//edit family details
-router.put('/v1/families/:id', familyController.family_edit);
+//edit group details
+router.put('/v1/groups/:id', groupsController.groups_edit);
 
 
-//delete family
-router.delete('/v1/families/:id', familyController.family_delete);
+//delete groups
+router.delete('/v1/groups/:id', groupsController.groups_delete);
 
 
 
